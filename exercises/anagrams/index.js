9d47c09482dfbc2f1b5bdfbd596a5b367eaa706d
Murad Zalov <mzalov@gmail.com>
2020-09-29 02:25:20 -0500

    completed anagrams and chunk

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+  let objA = {};
+  let objB = {};
+  let modifiedStringA = stringA.toLowerCase().replace(/[^\w]/g, '')
+  let modifiedStringB = stringB.toLowerCase().replace(/[^\w]/g, '')
+
+  const buildCharMap = (str, obj) => {
+    for (let i = 0; i < str.length; i++) {
+      if (obj[str[i]]) {
+        obj[str[i]]++
+      } else {
+        obj[str[i]] = 1
+      }
+    }
+    return objA
+  }
+
+  buildCharMap(modifiedStringA, objA)
+  buildCharMap(modifiedStringB, objB)
+
+  // Check to see if the objects' keys are different lengths; if they are, then we immediately know they're not anagrams, as anagrams must have the same key lengths
+  if (Object.keys(objA).length !== Object.keys(objB).length) {
+    return false
+  }
+
+  // Check to see if all values associated with every key are equal between both objects
+  for (let value in objA) {
+    if (objA[value] !== objB[value]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
